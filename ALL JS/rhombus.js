0fcb4e163ebb01c5539cf7b96aa51dffde194f23
unknown <sumaiya.rrashid@gmail.comm>
2023-02-18 13:31:24 +0100

    rhombus js add

---
+++ ALL JS/rhombus.js
@@ -0,0 +1,12 @@
+
+document.getElementById('rhombus-calculate').addEventListener('click', function(){
+    cunter+=1;
+      const resultArea = document.getElementById('Calculate-result')
+      const tr = document.createElement('tr');
+      tr.innerHTML= `<tr>
+      <td>${cunter}</td>
+      <td> ${areaOfTRP('rhombus-d1', 'rhombus-d2' )} </td>
+      <td><button class="btn btn-primary ml-2 position-relative bottom-50"> Convert </button></td> 
+      </tr>`
+      resultArea.appendChild (tr);
+  })
